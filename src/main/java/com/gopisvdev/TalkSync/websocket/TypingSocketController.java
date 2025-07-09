d4f71d40971e5346a53a2fd65520f1a13c5be74d
GopiSVDev <gopisv.webdev@gmail.com>
2025-07-09 16:46:19 +0530

    fix(websocket): add @MessageMapping(/typing) to enable typing status broadcasting

--- src/main/java/com/gopisvdev/TalkSync/websocket/TypingSocketController.java
+++ src/main/java/com/gopisvdev/TalkSync/websocket/TypingSocketController.java
@@ -2,6 +2,7 @@ package com.gopisvdev.TalkSync.websocket;
 
 import com.gopisvdev.TalkSync.dto.chat.TypingStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -14,7 +15,9 @@ public class TypingSocketController {
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
+    @MessageMapping("/typing")
     public void handleTyping(@Payload TypingStatus typingStatus) {
+        System.out.println("Typing status received: " + typingStatus);
         UUID chatId = typingStatus.getChatId();
         messagingTemplate.convertAndSend("/topic/chat/" + chatId + "/typing", typingStatus);
     }
