2a557c38db62f8e23db4cdcb15487388e001081f
GopiSVDev <gopisv.webdev@gmail.com>
2025-07-12 23:18:47 +0530

    Add Docker file

--- src/main/java/com/gopisvdev/TalkSync/config/EnvConfig.java
+++ src/main/java/com/gopisvdev/TalkSync/config/EnvConfig.java
@@ -1,15 +1,12 @@
 package com.gopisvdev.TalkSync.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class EnvConfig {
-    static {
-        Dotenv dotenv = Dotenv.configure().directory("./").load();
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-    }
-}
+//@Configuration
+//public class EnvConfig {
+//    static {
+//        Dotenv dotenv = Dotenv.configure().directory("./").load();
+//        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+//    }
+//}
--- src/main/java/com/gopisvdev/TalkSync/config/SecurityConfig.java
+++ src/main/java/com/gopisvdev/TalkSync/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://talksync-ten.vercel.app/"));
         config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
